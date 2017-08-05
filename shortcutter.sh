206b0f44fb9bcbf83c99f53cf8b7bef556f6eb6b
MateusMelo <mateusmelodasilva@gmail.com>
2017-08-05 02:34:18 -0300

    creating desktop shortcurt config file

--- shortcutter.sh
+++ shortcutter.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-FOLDER=$1
+EXEC=$1
+ICON=$2
+CONFIGFILE=${EXEC}.desktop
 RED='\033[0;32m'
 NC='\033[0m'
 
-echo -e "Creating shortcurt for ${RED}${FOLDER}${NC}"
+echo -e "Creating shortcurt for ${RED}${EXEC}${NC} \n"
+
+echo -e "Moving for /usr/local/bin \n"
+
+#sudo mv ${EXEC}/ /usr/local/bin/
+
+echo -e "Creating config file \n"
+
+touch ${CONFIGFILE}
+
+echo "[Desktop Entry]" >> ${CONFIGFILE}
+echo "Encoding=UTF-8" >> ${CONFIGFILE}
