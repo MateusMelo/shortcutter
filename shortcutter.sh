189bf278c53b0c578912d6a532f8f30c58443a93
MateusMelo <mateusmelodasilva@gmail.com>
2019-09-18 00:25:37 -0300

    Icon arg added

--- shortcutter.sh
+++ shortcutter.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-EXEC=$1
-ICON=$2
-CONFIGFILE=${EXEC}.desktop
-RED='\033[0;32m'
+FOLDER=$1
+NAME=$2
+ICON=$3
+CONFIGFILE=${NAME}.desktop
+GREEN='\033[0;32m'
 NC='\033[0m'
 
-echo -e "Creating shortcurt for ${RED}${EXEC}${NC} \n"
+echo -e "Creating shortcurt for ${GREEN}${NAME}${NC} \n"
 
-echo -e "Moving for /usr/local/bin \n"
+echo -e "Moving to /opt \n"
 
-#sudo mv ${EXEC}/ /usr/local/bin/
+sudo mv ${FOLDER}/ /opt/
+
+echo -e "Linking to /usr/local/bin \n"
+
+sudo chmod +x /opt/${NAME}/${NAME}
+
+sudo ln -s /opt/${NAME}/${NAME} /usr/local/bin/${NAME}
 
 echo -e "Creating config file \n"
 
@@ -18,3 +25,15 @@ touch ${CONFIGFILE}
 
 echo "[Desktop Entry]" >> ${CONFIGFILE}
 echo "Encoding=UTF-8" >> ${CONFIGFILE}
+echo "Name=${NAME}" >> ${CONFIGFILE}
+echo "Comment=Launch ${NAME}" >> ${CONFIGFILE}
+echo "Icon=${ICON}" >> ${CONFIGFILE}
+echo "Exec=/usr/local/bin/${NAME}" >> ${CONFIGFILE}
+echo "Terminal=false" >> ${CONFIGFILE}
+echo "Type=Application" >> ${CONFIGFILE}
+echo "Categories=Developer;" >> ${CONFIGFILE}
+echo "StartupNotify=true" >> ${CONFIGFILE}
+
+mv ${CONFIGFILE} ~/.local/share/applications/${CONFIGFILE}
+
+echo -e "Restart for apply all changes \n"
